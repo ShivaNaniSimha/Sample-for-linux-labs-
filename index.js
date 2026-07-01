ab5731353b30d993878d858024f203515202c001
Shiva Nani <146841609+ShivaNaniSimha@users.noreply.github.com>
2026-07-01 15:37:08 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send(`
+        <html>
+        <head>
+            <title>CI/CD Test</title>
+            <style>
+                body{
+                    font-family: Arial;
+                    text-align:center;
+                    margin-top:100px;
+                    background:#f4f4f4;
+                }
+                h1{
+                    color:#2c3e50;
+                }
+                p{
+                    font-size:22px;
+                }
+            </style>
+        </head>
+
+        <body>
+            <h1>🚀 CI/CD Automation Test</h1>
+            <p>Application Version : <b>Version 1.0</b></p>
+            <p>If you can see this page, deployment was successful.</p>
+        </body>
+        </html>
+    `);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
